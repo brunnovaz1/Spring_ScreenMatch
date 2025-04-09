0bace413832afc756998d74b6115351fed2d713b
brunnovaz1 <brunnovaz@gmail.com>
2025-04-09 15:46:17 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,20 @@
+package br.com.alura.screenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {   //implementacao necessaria para rodar código
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!!!");
+
+	}
+}
