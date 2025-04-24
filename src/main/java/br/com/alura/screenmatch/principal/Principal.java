bb07b3277317a7580df096022e772e3bbfbabe20
brunnovaz1 <brunnovaz@gmail.com>
2025-04-24 16:03:05 -0300

    Selecionando apenas titulos

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
@@ -26,7 +27,7 @@ public class Principal {
 
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 
-        System.out.println(dados);
+       // System.out.println(dados);
 
         List<DadosTemporada> temporadas = new ArrayList<>();
 
@@ -36,5 +37,13 @@ public class Principal {
 			temporadas.add(dadosTemporada);
 		}
 		temporadas.forEach(System.out::println);
+
+//        for(int i = 0; i < dados.totalTemporadas(); i++){
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for(int j = 0; j < episodiosTemporada.size(); j++){
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
     }
 }
