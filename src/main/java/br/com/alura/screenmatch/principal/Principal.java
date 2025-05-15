74e30d339fa52f2d73c59c8c9cde605a0bf0f3ba
brunnovaz1 <brunnovaz@gmail.com>
2025-05-15 17:10:30 -0300

    Optional

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -9,10 +9,7 @@ import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -42,15 +39,17 @@ public class Principal {
 			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
 			temporadas.add(dadosTemporada);
 		}
-		temporadas.forEach(System.out::println);
+        System.out.println("- - - - - - - - - - - - - - - - -");
 
-//        for(int i = 0;  i < dados.totalTemporadas(); i++){
-//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-//            for (int j = 0; j<episodiosTemporada.size(); j++ ){
-//                System.out.println(episodiosTemporada.get(j).titulo());
-//            }
-//        }
-        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+//		temporadas.forEach(System.out::println);
+
+        for(int i = 0;  i < dados.totalTemporadas(); i++){
+            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            for (int j = 0; j<episodiosTemporada.size(); j++ ){
+ //               System.out.println(episodiosTemporada.get(j).titulo());
+            }
+        }
+//        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
 
         List<DadosEpisodio> dadosEpisodios = temporadas.stream()
                 .flatMap(t -> t.episodios().stream())
@@ -58,20 +57,35 @@ public class Principal {
         //tenho uma lista dentro de outra e quero puxar todos dados juntos
         //.toList(); cria uma lista imutável
 
-        System.out.println("\nTOP-5 Episódios");
+ /*       System.out.println("\nTOP-5 Episódios");
         dadosEpisodios.stream()
                 .filter(e -> !e.avaliacao().equals("N/A"))
                 .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
                 .limit(5)
                 .forEach(System.out::println);
-
+*/
         List<Episodio> episodios = temporadas.stream()
                 .flatMap(t -> t.episodios().stream()
                 .map(dadosEpisodio -> new Episodio(t.numero(), dadosEpisodio))
                 ).collect(Collectors.toList());
 
         episodios.forEach(System.out::println);
-        System.out.println("A partir de que ano você quer os espisodios?");
+
+        System.out.println(" - - - - - - - - - - - - - - - -");
+        System.out.print("Busca pelo título: ");
+        String trechoTitulo = scan.nextLine();
+        Optional<Episodio> episodioBuscado = episodios.stream()
+                .filter(episodio -> episodio.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+                .findFirst();
+        if(episodioBuscado.isPresent()){
+            System.out.println("Temporada: " + episodioBuscado.get().getTemporada() +
+            " - Episódio: " + episodioBuscado.get().getNumero());
+        } else {
+            System.out.println("Episódio não encontrado");
+        }
+
+
+ /*     System.out.println("A partir de que ano você quer os espisodios?");
         int ano = scan.nextInt();
         scan.nextLine();
         DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
@@ -83,6 +97,6 @@ public class Principal {
                         " - Episódio: " + episodio.getTitulo() +
                         " - Data Lançamento: " + episodio.getDataLancamento().format(formatador)
                 ));
-
+*/
     }
 }
