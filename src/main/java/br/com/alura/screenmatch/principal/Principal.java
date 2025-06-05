48d0ee4b9305f6daff7bff3495644a002797b0fa
brunnovaz1 <brunnovaz@gmail.com>
2025-06-05 16:57:10 -0300

    busca de episodio por termos e  DoubleSummaryStatistics

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -11,10 +11,7 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -56,7 +53,7 @@ public class Principal {
                 .flatMap(t -> t.episodios().stream()
                     .map(d -> new Episodio(t.numero(), d ))
                 ).collect(Collectors.toList());
-       // episodios.forEach(System.out::println);
+    //    episodios.forEach(System.out::println);
 
         List<Episodio> top5 = episodios.stream()
                 .sorted(Comparator.comparing(Episodio::getAvaliacao).reversed())
@@ -65,7 +62,7 @@ public class Principal {
         //System.out.println("\n                                  +++++ TOP-5 +++++");
         //top5.forEach(System.out::println);
 
-        System.out.println("--==-- Busca Episódio por data --==--");
+    /*    System.out.println("--==-- Busca Episódio por data --==--");
         System.out.print("Ano: ");
         int ano = scan.nextInt();
         scan.nextLine();
@@ -85,5 +82,40 @@ public class Principal {
                     );
                     System.out.println(linha);
                 });
+
+
+        System.out.println("--==-- Busca Episódio por nome/termo --==--");
+        System.out.print("Nome: ");
+        String trechoTitulo = scan.nextLine();
+        Optional<Episodio> episodioBuscado = episodios.stream()
+                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+                .findFirst();
+        if(episodioBuscado.isPresent()) {
+            System.out.println(episodioBuscado);
+        } else{
+            System.out.println("Espisódio não encontrado!");
+        }
+
+        System.out.println("--== Média Temporadas ==--");
+        Map<Integer, Double> avaliacaoPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,           //agrupa por temporada
+                        Collectors.averagingDouble(Episodio::getAvaliacao)));    //media de avaliacoes
+        avaliacaoPorTemporada.forEach(
+                (temporada, media) -> {
+                    String resp = String.format("Temporada:%d - Média:%.1f%n", temporada, media);
+                    System.out.print(resp);
+                }
+        );
+
+        */
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+        System.out.println(
+                "Nº Episódios: "+ est.getCount() +
+                "\nMaior nota: "+ est.getMax()   +
+                "\nPior nota: " + est.getMin()
+        );
     }
 }
